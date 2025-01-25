2965a00880152cac45688208e57d0d01b544b293
Igor Cwiertnia <reallia13@gmail.com>
2025-01-25 13:36:47 +0100

    [HOOK] Automatyczny commit z pre-commit

---
+++ skrypty/auto-tag.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Pobierz ostatni tag
+LAST_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "v0.0.0")
+echo "Ostatnia wersja: $LAST_TAG"
+
+# Zwiększ wersję patch (ostatnia cyfra)
+NEW_TAG=$(echo $LAST_TAG | awk -F. '{printf "v%d.%d.%d", $1, $2, $3+1}')
+echo "Nowa wersja: $NEW_TAG"
+
+# Tworzenie nowego tagu
+git tag -a "$NEW_TAG" -m "Nowa wersja: $NEW_TAG"
+git push origin "$NEW_TAG"
